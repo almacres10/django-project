7823399b008cf62f1dba6538420266fb91b3b9f5
Administrator TIK <pajak@NB050272KEP02.intranet.pajak.go.id>
2023-03-11 23:23:46 +0700

    update 11 maret 2023

--- base/views.py
+++ base/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 
@@ -175,5 +175,14 @@ def deleteMessage(request, pk):
 
 @login_required(login_url='login')
 def updateUser(request):
-    return render(request, 'base/update_user.html')
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile, pk=user.id') 
+
+    return render(request, 'base/update_user.html', {'form':form})
 
